a14da7fd4ac31224b460f76ff9f0abdadb0854d8
Giorgio Gamberoni <giorgio.gamberoni@gmail.com>
2016-11-17 09:15:39 +0100

    Add files via upload

---
+++ tests/unit/helpers/completed-todo-test.js
@@ -0,0 +1,12 @@
+
+import { completedTodo } from 'admin-forum-ember/helpers/completed-todo';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | completed todo');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = completedTodo([42]);
+  assert.ok(result);
+});
+
---
+++ tests/unit/routes/user/detail/todos-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:user/detail/todos', 'Unit | Route | user/detail/todos', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
